057e4cc1c9bed6d4d0b5366bea147accd70897a8
Semin Kwak <semin.kwak@epfl.ch>
2018-10-05 18:16:12 +0200

    Added wall

--- intro/kwak/main.js
+++ intro/kwak/main.js
@@ -1,51 +1,31 @@
+// Import graph.js and wall.js
+import wall from "./wall.js";
+import graph from "./graph.js";
+
+
 // Define Height and width for SVG canvas
-var margin = {top: 10, right: 30, bottom: 30, left: 30},
+const margin = {top: 10, right: 30, bottom: 30, left: 30},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
 // SVG canvas
-var svg = d3.select("body").append("svg")
+let svg = d3.select("body").append("svg")
     .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    .attr("height", height + margin.top + margin.bottom);
 
-// Background
-svg.append("rect")
-    .attr("width", "100%")
-    .attr("height", "100%")
-    .attr("fill", "gray");
+let group = svg
+    .append("g");
 
-// Add some text. (CSS for the text)
-// But compute the width of the text
-var widthText = 0;
+wall["walls"].map( (w)  => {
+    group.append("line")
+        .attr("class", "the-line")
+        .attr("x1", w["x1"])
+        .attr("y1", w["y1"])
+        .attr("x2", w["x2"])
+        .attr("y2", w["y2"])
+}) ;
 
-var text = svg.append("text")
-        .attr("x", width/2)
-        .attr("y", height/2)
-        .text( function(d) { return "Have fun with d3.js!"})
-        .attr("text-anchor", "middle")
-        .attr("id", "middle-text")
-        .each(function(d,i) {
-            widthText = Math.round(1.2*this.getComputedTextLength());
-        })
-        ;
-
-console.log(widthText);
-
-// Draw a rectangle in the middle of the canvas
-// with the size of the text
-var wRect = widthText,
-    hRect = 100;
-
-// Warning, you need to insert the rectangle since
-// it has to come before the text and not after (--> append)
-svg.insert("rect", "text")
-        .attr("width", wRect)
-        .attr("height", hRect)
-        .attr("x", width/2 - wRect/2)
-        .attr("y", height/2 - hRect/2)
-        .attr("fill", "red")
-        .attr("id", "rect");
+group.attr("transform", `translate( ${margin.left + width/2}, ${margin.top + height/2})`)
+    .attr("transform", "scale(10)");
 
 
