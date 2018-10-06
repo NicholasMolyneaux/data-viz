90866becb45867854bc5fce6c5a6a1b87ea86ba6
Semin Kwak <seminkwak@Seminui-MacBook-Air.local>
2018-10-06 12:45:13 +0200

    fitted walls to canvas

--- intro/kwak/main.js
+++ intro/kwak/main.js
@@ -2,30 +2,58 @@
 import wall from "./wall.js";
 import graph from "./graph.js";
 
+const x_offset = Math.min(...wall["walls"].map( (w) => Math.min(w["x1"], w["x2"]) ));
+const y_offset = Math.min(...wall["walls"].map( (w) => Math.min(w["y1"], w["y2"]) ));
+const x_length = Math.max(...wall["walls"].map( (w) => Math.max(w["x1"], w["x2"]) )) - x_offset;
+const y_length = Math.max(...wall["walls"].map( (w) => Math.max(w["y1"], w["y2"]) )) - y_offset;
+const xy_ratio = y_length/x_length;
 
 // Define Height and width for SVG canvas
-const margin = {top: 10, right: 30, bottom: 30, left: 30},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+const width = 500;
+const height = xy_ratio*width;
+const scale = width/x_length;
 
 // SVG canvas
 let svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom);
+    .attr("width", width)
+    .attr("height", height);
 
-let group = svg
+// Border
+let border = svg.append("rect")
+    .attr("class", "canvas-border")
+    .attr("x", 0)
+    .attr("y", 0)
+    .attr("width", width)
+    .attr("height", height);
+
+// Draw walls
+let group_walls = svg
     .append("g");
-
 wall["walls"].map( (w)  => {
-    group.append("line")
-        .attr("class", "the-line")
-        .attr("x1", w["x1"])
-        .attr("y1", w["y1"])
-        .attr("x2", w["x2"])
-        .attr("y2", w["y2"])
+    group_walls.append("line")
+        .attr("class", "the-walls")
+        .attr("x1", w["x1"]- x_offset)
+        .attr("y1", w["y1"]- y_offset)
+        .attr("x2", w["x2"]- x_offset)
+        .attr("y2", w["y2"]- y_offset);
 }) ;
+group_walls.attr("transform", `translate( ${width/2 }, ${height/2 })`)
+    .attr("transform", `scale(${scale})`);
 
-group.attr("transform", `translate( ${margin.left + width/2}, ${margin.top + height/2})`)
-    .attr("transform", "scale(10)");
+// Draw zones
+// let group_zones = svg
+//     .append("g");
+// graph["nodes"].map( (g) => {
+//     group_zones.append("rect")
+//         .attr("class", "the-zones")
+//         .attr("x", g["x"]-x_offset)
+//         .attr("y", g["y"]-y_offset)
+//         .attr("width", g["x2"]-g["x1"])
+//         .attr("height", g["y3"]-g["y3"]);
+//         console.log(g["x"]-x_offset)
+// });
 
+// group_zones.attr("transform", `translate( ${width/2 }, ${height/2 })`)
+//     .attr("transform", `scale(${scale})`);
 
+// Draw pedestrians
