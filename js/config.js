90f3d548eb6b07d99d3fb4c9bda72c1212b9671a
Nicholas Molyneaux <nicholas.molyneaux@epfl.ch>
2021-02-25 11:51:39 +0100

    Created config.js based upon AWS instructions

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'eu-west-3_AHJkvc7rs', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7ca9nlrp36sba96qv62d4r2g04', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-3' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
